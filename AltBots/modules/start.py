08e654491624cc3ba8da23830ac45db3f76c60da
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2024-07-07 09:37:30 +0530

    Update start.py

--- AltBots/modules/start.py
+++ AltBots/modules/start.py
@@ -8,8 +8,8 @@ START_BUTTON = [
         Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")
     ],
     [
-        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/team_nexz"),
-        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/nexz_support")
+        Button.url("• ᴄʜᴀɴɴᴇʟ •", "https://t.me/+tS0zXjrYPypjMGJl"),
+        Button.url("• sᴜᴘᴘᴏʀᴛ •", "https://t.me/+vqDQ2VZjafRkNWE1")
     ],
     
 ]
@@ -32,13 +32,13 @@ async def start(event):
         bot_name = AltBot.first_name
         bot_id = AltBot.id
         TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n━━━━━━━━━━━━━━━━━━━\n\n"
-        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [ɴᴇxᴢ](https://t.me/nexzz_op)**\n\n"
+        TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝗗ᴇsᴛʀᴏʏᴇʀ](https://t.me/ll_Destroyer_of_worldd_ll)**\n\n"
         TEXT += f"» **xʙᴏᴛꜱ ᴠᴇʀsɪᴏɴ :** `M3.3`\n"
         TEXT += f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
         TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n━━━━━━━━━━━━━━━━━"
         await event.client.send_file(
                     event.chat_id,
-                    "https://telegra.ph/file/719e7092b9a37a100468d.jpg",
+                    "https://telegra.ph/file/197db5af69263b1ccd155.jpg",
                     caption=TEXT, 
                     buttons=START_BUTTON
                 )
